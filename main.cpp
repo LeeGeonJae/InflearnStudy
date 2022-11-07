58bed1f43209acb34cb2a88eb3dac7c560c89da1
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-07 23:11:07 +0900

    스택 오버플로우

--- main.cpp
+++ main.cpp
@@ -2,36 +2,42 @@
 
 using namespace std;
 
-// 오늘의 주제 : 호출 스택
+// 오늘의 주제 : 함수 마무리
 
-// 함수 선언
-void Func1();
-void Func2(int a, int b);
-void Func3(float a);
+// 오버로딩 ( 중복 정의 : 함수 이름 재사용 )
+// - 매개변수 갯수가 다르거나
+// - 매개변수 타입이 다르거나 (순서가 다른걸 포함)
+int Add(int a, int b)
+{
+	int result = a + b;
+	return result;
+}
+
+float Add(float a, float b)
+{
+	float result = a + b;
+	return result;
+}
+
+void SetPlayerInto(int hp, int mp, int attack, int guildId = 1, int castleId = 2)
+{
+
+}
+
+// 스택 오버플로우
+
+int Factorial(int n)
+{
+	if (n <= 1)
+		return 1;
+	return n * Factorial(n - 1);
+}
 
 int main()
 {
-	cout << "main" << endl;
-	Func1();
+	int result = Factorial(5);
+	cout << result << endl;
 
 	return 0;
 }
 
-void Func3(float a)
-{
-	cout << "Func3" << endl;
-
-	// push 2
-	// push 1
-	// call Func2
-	Func2(1, 2);
-}
-
-
-
-void Func1()
-{
-	cout << "Func1" << endl;
-
-	Func2(1, 2);
-}
