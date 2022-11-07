9c5398ed10e2bed9e06b9527a18628df2b4d692e
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-07 18:17:37 +0900

    호출 스택

--- main.cpp
+++ main.cpp
@@ -2,29 +2,12 @@
 
 using namespace std;
 
+// 오늘의 주제 : 호출 스택
 
-int GlobalValue = 0;
-
-void Func3 (float a)
-{
-	cout << "Func3" << endl;
-}
-
-void Func2(int a, int b)
-{
-	cout << "Func2" << endl;
-
-	Func3(10);
-}
-
-void Func1()
-{
-	cout << "Func1" << endl;
-
-	Func2(1, 2);
-}
-
-
+// 함수 선언
+void Func1();
+void Func2(int a, int b);
+void Func3(float a);
 
 int main()
 {
@@ -32,4 +15,23 @@ int main()
 	Func1();
 
 	return 0;
+}
+
+void Func3(float a)
+{
+	cout << "Func3" << endl;
+
+	// push 2
+	// push 1
+	// call Func2
+	Func2(1, 2);
+}
+
+
+
+void Func1()
+{
+	cout << "Func1" << endl;
+
+	Func2(1, 2);
 }
