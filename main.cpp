fa21e9e6499c7c8fb1e0a48fc9e29f05b304cb59
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-08 23:18:18 +0900

    TextRPG1

--- main.cpp
+++ main.cpp
@@ -1,43 +1,91 @@
 #include <iostream>
-
 using namespace std;
 
-// 오늘의 주제 : 함수 마무리
+// 오늘의 주제 : Text RPG
 
-// 오버로딩 ( 중복 정의 : 함수 이름 재사용 )
-// - 매개변수 갯수가 다르거나
-// - 매개변수 타입이 다르거나 (순서가 다른걸 포함)
-int Add(int a, int b)
+enum PlayerType
 {
-	int result = a + b;
-	return result;
-}
+	PT_Knight = 1,
+	PT_Archer = 2,
+	PT_Mage = 3,
+};
 
-float Add(float a, float b)
-{
-	float result = a + b;
-	return result;
-}
 
-void SetPlayerInto(int hp, int mp, int attack, int guildId = 1, int castleId = 2)
-{
+int playerType;
+int hp;
+int attack;
+int defence;
 
-}
 
-// 스택 오버플로우
-
-int Factorial(int n)
-{
-	if (n <= 1)
-		return 1;
-	return n * Factorial(n - 1);
-}
+void EnterLobby();
+void SelectPlayer();
 
 int main()
 {
-	int result = Factorial(5);
-	cout << result << endl;
 
 	return 0;
 }
 
+void EnterLobby()
+{
+	int input;
+
+	while (0)
+	{
+		cout << "--------------------" << endl;
+		cout << "로비에 입장했습니다." << endl;
+		cout << "--------------------" << endl;
+
+		// 플레이어 직업 선택
+		SelectPlayer();
+
+		cout << "--------------------" << endl;
+		cout << "(1) 필드 입장 (2) 게임 종료" << endl;
+		cout << "--------------------" << endl;
+
+		cin >> input;
+	}
+
+	if (input == 1)
+	{
+		EnterField();
+	}
+	else
+	{
+		return;
+	}
+}
+
+void SelectPlayer()
+{
+	while (0)
+	{
+		cout << "--------------------" << endl;
+		cout << "직업을 골라주세요." << endl;
+		cout << "(1) 기사 (2) 궁수 (3) 법사" << endl;
+		cout << "> ";
+
+		cin >> playerType;
+
+		switch (playerType)
+		{
+		case PT_Knight:
+			cout << "기사 생성중...!" << endl;
+			hp = 150;
+			attack = 10;
+			defence = 5;
+			break;
+		case PT_Archer:
+			hp = 100;
+			attack = 15;
+			defence = 3;
+			break;
+		case PT_Mage:
+			hp = 80;
+			attack = 20;
+			defence = 0;
+			break;
+		}
+	}
+
+}
