5d6cc57ed5b7875aad8b35bcfd3a9dfbfbd92fb5
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-11-30 15:25:40 +0900

    이중 포인터

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ void SetMessage(const char* a)
 
 void SetMassage(const char** a)
 {
-	// const char* msg의 주소값을 "Bye" 주소값으로 바꾼다.
+	// const char* msg의 주소값을 "Bye" 를 향하는 주소값으로 바꾼다.
 	*a = "Bye";
 }
 
@@ -29,7 +29,7 @@ int main()
 {
 	int a = 0;
 	SetNumber(&a);
-	//cout << a << endl;
+	cout << a << endl;
 
 	// .rdata	Hello주소[H][e][l][l][o][\0]
 	// .rdata	Bye주소[B][y][e][\0]
@@ -38,7 +38,7 @@ int main()
 
 	// [매개변수][RET][지역변수(msg(Hello주소))][매개변수(a(Bye주소))][RET][지역변수]
 	SetMessage(msg);
-	// cout << msg << endl; 
+	cout << msg << endl; 
 
 	// .rdata	Hello주소[H][e][l][l][o][\0]
 	// 주소1[ Hello주소 ] << 8바이트
@@ -48,7 +48,7 @@ int main()
 	// [매개변수][RET][지역변수(msg(Hello주소))][매개변수(a(msg의 주소))][RET][지역변수]
 	SetMassage(&msg);
 	cout << msg << endl;
-	
+
 	// 다중 포인터 : 혼동스럽다?
 	// 그냥 양파까기라고 생각하면 된다
 	// *을 하나씩 까면서 타고 간다고 생각하면 된다.
