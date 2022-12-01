2985dbb9c798cc5cf0a2a2c9a9326b7f34f78086
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-12-01 20:17:51 +0900

    2차원 배열 vs 다중 포인터

--- main.cpp
+++ main.cpp
@@ -2,65 +2,89 @@
 
 using namespace std;
 
-// 오늘의 주제 : 다차원 배열
+// 오늘의 주제 : 포인터 마무리
 
+// 1) 포인터 vs 배열 2탄
+// 2) 주의사항 (마음가짐?)
+
+int& TestRef()
+{
+	int a = 1;
+	return a;
+}
+
+int* TestPointer()
+{
+	int a = 1;
+	return &a;
+}
+
+void TestWrong(int* ptr)
+{
+	int a[100] = {};
+	a[99] = 0xAAAAAAAA;
+	*ptr = 0x12341234;
+}
 
 int main()
 {
-	int a[10] = { 1,2,3 };
+	// 주소를 담는 바구니
+	// 진퉁은 저~ 멀리 어딘가에 있음
+	// p는 단지 그 곳으로 워프하는 포탈
+	int* p;
 
-	// [ ] [ ] [ ] [ ] [ ] << 0층
-	// [ ] [ ] [ ] [ ] [ ] << 1층
+	// 진짜배기! 원조 데이터
+	// 닭장처럼 데이터의 묶음 (엄청 많고 거대함)
+	int arr[10] = { 1,2,3,4,5,6,7,8 };
 
-	int first[5] = { 4,2,3,4,1 };
-	int second[5] = { 1,1,5,2,4 };
+	// 그런데 상당히 많은 사람들이 [배열 = 포인터]라 착각하는 경향이 있음!
 
-	int apartment2D[2][5] = { {4,2,3,4,1},{1,1,5,2,2} };
+	// - [배열의 이름]은 배열의 시작 주소값을 가리키는 TYPE* 포인터로 변환 가능!
+	p = arr;
 
-	for (int floor = 0; floor < 2; floor++)
-	{
-		for (int room = 0; room < 5; room++)
-		{
-			// apartment2D + (floor * 20) + 4 * room를 한 주소
-			int num = apartment2D[floor][room];
-			cout << num << " ";
-		}
-		cout << endl;
-	}
+	// - [TYPE형 1차원 배열]과 [TYPE*형 포인터]는 완전히 호환이 된다
+	cout << p[0] << endl;
+	cout << arr[0] << endl;
+	cout << p[5] << endl;
+	cout << arr[5] << endl;
+	cout << *p << endl;
+	cout << *arr << endl;
+	cout << *(p + 3) << endl;
+	cout << *(arr + 3) << endl;
 
-	int apartment1D[10] = { 4,2,3,4,1,1,1,5,2,2 };
+	// 2차원 배열 vs 다중 포인터
 
-	for (int floor = 0; floor < 2; floor++)
-	{
-		for (int room = 0; room < 5; room++)
-		{
-			int index = (floor * 5) + room;
-			// apartment1D + (floor * 20) + 4 * room을 한 주소
-			int num = apartment1D[index];
-			cout << num << " ";
-		}
-		cout << endl;
-	}
+	// [1][2][3][4]
+	int arr2[2][2] = { {1,2},{3,4} };
 
-	// 2차 배열은 언제 사용할까? 대표적으로 2D 로그라이크 맵
-	int map[5][5] =
-	{
-		{1,1,1,1,1},
-		{1,0,0,1,1},
-		{0,0,0,0,1},
-		{1,0,0,0,0},
-		{1,1,1,1,1},
-	};
+	// 주소1[1][2][3][4]
+	// pp[ 주소1 ] 
 
-	for (int y = 0; y < 5; y++)
-	{
-		for (int x = 0; x < 5; x++)
-		{
-			int info = map[y][x];
-			cout << info;
-		}
-		cout << endl;
-	}
+	// TYPE name[개수]
+
+	// [1][2] [3][4]
+	// [ 주소 ]
+	int(*p2)[2] = arr2;
+	cout << (*p2)[0] << endl;
+	cout << (*p2)[1] << endl;
+	cout << (*(p2 + 1))[0] << endl;
+	cout << (*(p2 + 1))[1] << endl;
+
+	cout << p2[0][0] << endl;
+	cout << p2[0][1] << endl;
+	cout << p2[1][0] << endl;
+	cout << p2[1][1] << endl;
+
+	// [매개변수][RET][지역변수]
+	int* pointer = TestPointer();
+
+	cout << *pointer << endl;
+
+	TestWrong(pointer);
+	cout << *pointer << endl;
+
+	*pointer = 123;
+	cout << *pointer << endl;
 
 	return 0;
 }
