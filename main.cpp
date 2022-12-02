ef4f68030b9aae376a31de21076abddaceb6f30c
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-12-02 15:10:19 +0900

    TextRPG#3

--- main.cpp
+++ main.cpp
@@ -2,89 +2,219 @@
 
 using namespace std;
 
-// 오늘의 주제 : 포인터 마무리
+// 오늘의 주제 : TEXTRPG2
 
-// 1) 포인터 vs 배열 2탄
-// 2) 주의사항 (마음가짐?)
 
-int& TestRef()
+//main
+// - EnterLobby (PlayerInfo)
+// -- CreatePlayer
+// -- EnterGame (MonsterInfo)
+// --- CreateMonsters
+// --- EnterBattle
+
+enum PlayerType
 {
-	int a = 1;
-	return a;
-}
+	PT_Knight = 1,
+	PT_Archer = 2,
+	PT_Mage = 3,
+};
 
-int* TestPointer()
+enum MonsterType
 {
-	int a = 1;
-	return &a;
-}
+	MT_Slime = 1,
+	MT_Orc = 2,
+	MT_Skeleton = 3,
+};
 
-void TestWrong(int* ptr)
+struct StatInfo
 {
-	int a[100] = {};
-	a[99] = 0xAAAAAAAA;
-	*ptr = 0x12341234;
-}
+	int hp = 0;
+	int attack = 0;
+	int defence = 0;
+};
+
+void EnterLobby();
+void PrintMassege(const char* msg);
+void CreatePlayer(StatInfo* playerInfo);
+void PrintStatInfo(const char* name, const StatInfo& info);
+void EnterGame(StatInfo* playerInfo);
+void CreateMonsters(StatInfo monsterInfo[], int Count);
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo);
+
 
 int main()
 {
-	// 주소를 담는 바구니
-	// 진퉁은 저~ 멀리 어딘가에 있음
-	// p는 단지 그 곳으로 워프하는 포탈
-	int* p;
-
-	// 진짜배기! 원조 데이터
-	// 닭장처럼 데이터의 묶음 (엄청 많고 거대함)
-	int arr[10] = { 1,2,3,4,5,6,7,8 };
-
-	// 그런데 상당히 많은 사람들이 [배열 = 포인터]라 착각하는 경향이 있음!
-
-	// - [배열의 이름]은 배열의 시작 주소값을 가리키는 TYPE* 포인터로 변환 가능!
-	p = arr;
-
-	// - [TYPE형 1차원 배열]과 [TYPE*형 포인터]는 완전히 호환이 된다
-	cout << p[0] << endl;
-	cout << arr[0] << endl;
-	cout << p[5] << endl;
-	cout << arr[5] << endl;
-	cout << *p << endl;
-	cout << *arr << endl;
-	cout << *(p + 3) << endl;
-	cout << *(arr + 3) << endl;
-
-	// 2차원 배열 vs 다중 포인터
-
-	// [1][2][3][4]
-	int arr2[2][2] = { {1,2},{3,4} };
-
-	// 주소1[1][2][3][4]
-	// pp[ 주소1 ] 
-
-	// TYPE name[개수]
-
-	// [1][2] [3][4]
-	// [ 주소 ]
-	int(*p2)[2] = arr2;
-	cout << (*p2)[0] << endl;
-	cout << (*p2)[1] << endl;
-	cout << (*(p2 + 1))[0] << endl;
-	cout << (*(p2 + 1))[1] << endl;
-
-	cout << p2[0][0] << endl;
-	cout << p2[0][1] << endl;
-	cout << p2[1][0] << endl;
-	cout << p2[1][1] << endl;
-
-	// [매개변수][RET][지역변수]
-	int* pointer = TestPointer();
-
-	cout << *pointer << endl;
-
-	TestWrong(pointer);
-	cout << *pointer << endl;
-
-	*pointer = 123;
-	cout << *pointer << endl;
+	srand((unsigned)time(nullptr));
+	EnterLobby();
 
 	return 0;
+}
+
+void EnterLobby()
+{
+	while (true)
+	{
+		PrintMassege("로비에 입장했습니다");
+
+		// PLAYER
+		// 스택 프레임에 의해 앞에 있는 스택 메모리에 있는 함수에서 다음 함수에게 넘기는 것은 괜찮지만
+		// 함수에서 생성되어 이전 함수에게 넘겨주는 것은 위험하다. 
+		// ( 스택 프레임에 의해 함수가 사용되고 난 후에 메모리가 없어지기 때문 )
+		StatInfo playerInfo;
+		CreatePlayer(&playerInfo);
+		PrintStatInfo("Player", playerInfo);
+
+		EnterGame(&playerInfo);
+	}
+}
+
+void PrintMassege(const char* msg)
+{
+	cout << "************************" << endl;
+	cout << msg << endl;
+	cout << "************************" << endl;
+}
+
+void CreatePlayer(StatInfo* playerInfo)
+{
+	bool ready = false;
+
+	while (ready == false)
+	{
+		PrintMassege("캐릭터 생성창");
+		PrintMassege("[1] 기사 [2] 궁수 [3] 법사");
+		cout << "> ";
+
+		int input;
+		cin >> input;
+
+		switch (input)
+		{
+		case PT_Knight:
+			playerInfo->hp = 100;
+			playerInfo->attack = 10;
+			playerInfo->defence = 5;
+			ready = true;
+			break;
+		case PT_Archer:
+			playerInfo->hp = 80;
+			playerInfo->attack = 15;
+			playerInfo->defence = 3;
+			ready = true;
+			break;
+		case PT_Mage:
+			playerInfo->hp = 50;
+			playerInfo->attack = 25;
+			playerInfo->defence = 1;
+			ready = true;
+			break;
+		default:
+			break;
+		}
+
+	}
+}
+
+void PrintStatInfo(const char* name, const StatInfo& info)
+{
+	cout << "**********************" << endl;
+	cout << name << " : HP=" << info.hp << " ATT=" << info.attack << " DEF=" << info.defence << endl;
+	cout << "**********************" << endl;
+}
+
+void EnterGame(StatInfo* playerInfo)
+{
+	const int MONSTER_COUNT = 2;
+
+	PrintMassege("게임에 입장했습니다");
+
+	while (true)
+	{
+		StatInfo monsterInfo[MONSTER_COUNT];
+		CreateMonsters(monsterInfo, MONSTER_COUNT);
+
+		for (int i = 0; i < MONSTER_COUNT; i++)
+			PrintStatInfo("Monster", monsterInfo[i]);
+
+		PrintStatInfo("Player", *playerInfo);
+
+		PrintMassege("[1] 전투 [2] 전투 [3] 도망");
+		int input;
+		cin >> input;
+
+		if (input == 1 || input == 2)
+		{
+			int index = input - 1;
+			bool victory = EnterBattle(playerInfo, &(monsterInfo[index]));
+			if (victory == false)
+				break;
+		}
+	}
+}
+
+void CreateMonsters(StatInfo monsterInfo[], int Count)
+{
+	for (int i = 0; i < Count; i++)
+	{
+		int randValue = 1 + (rand() % 3);
+
+		switch (randValue)
+		{
+		case MT_Slime:
+			monsterInfo[i].hp = 30;
+			monsterInfo[i].attack = 5;
+			monsterInfo[i].defence = 1;
+			break;
+		case MT_Orc:
+			monsterInfo[i].hp = 40;
+			monsterInfo[i].attack = 8;
+			monsterInfo[i].defence = 2;
+			break;
+		case MT_Skeleton:
+			monsterInfo[i].hp = 50;
+			monsterInfo[i].attack = 15;
+			monsterInfo[i].defence = 3;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo)
+{
+	while (true)
+	{
+		int damage = playerInfo->attack - monsterInfo->defence;
+		if (damage < 0)
+			damage = 0;
+
+		monsterInfo->hp -= damage;
+		if (monsterInfo->hp < 0)
+			monsterInfo->hp = 0;
+
+		PrintStatInfo("Monster", *monsterInfo);
+
+		if (monsterInfo->hp == 0)
+		{
+			PrintMassege("몬스터를 처치했습니다");
+			return true;
+		}
+
+		damage = monsterInfo->attack - playerInfo->defence;
+		if (damage < 0)
+			damage = 0;
+
+		playerInfo->hp -= damage;
+		if (playerInfo->hp < 0)
+			playerInfo->hp = 0;
+
+		PrintStatInfo("Player", *playerInfo);
+
+		if (playerInfo->hp == 0)
+		{
+			PrintMassege("Game Over");
+			return false;
+		}
+	}
 }
