450186fe49fcff5991dcf1ea90d03f79baa11e8f
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-12-02 18:51:05 +0900

    파일 분할 정리

--- main.cpp
+++ main.cpp
@@ -1,119 +1,15 @@
 #include <iostream>
-#include <iomanip>
+#include "Test1.h"
 
 using namespace std;
 
-// 오늘의 주제 : 연습 문제(달팽이)
+// 오늘의 주제 : 파일 분할 관리
 
-const int MAX = 100;
-int board[MAX][MAX] = {};
-int N;
-
-void PrintBoard()
-{
-	for (int y = 0; y < N; y++)
-	{
-		for (int x = 0; x < N; x++)
-		{
-			cout << setfill('0') << setw(2) << board[y][x] << " ";
-		}
-		cout << endl;
-	}
-}
-
-enum DIR
-{
-	RIGHT = 0,
-	DOWN = 1,
-	LEFT = 2,
-	UP = 3,
-};
-
-bool CanGo(int y, int x)
-{
-	if (y < 0 || y >= N)
-		return false;
-	if (x < 0 || x >= N)
-		return false;
-	if (board[y][x] != 0)
-		return false;
-	return true;
-}
-
-void SetBoard()
-{
-	// 1 2 3 4 5
-	//         6
-	// 
-	int dir = RIGHT;
-	int num = 1;
-	int y = 0;
-	int x = 0;
-	
-	while (true)
-	{
-		board[y][x] = num;
-
-		if (num == N * N)
-			break;
-
-		int nextY;
-		int nextX;
-
-		switch (dir)
-		{
-		case RIGHT:
-			nextY = y;
-			nextX = x + 1;
-			break;
-		case DOWN:
-			nextY = y + 1;
-			nextX = x;
-			break;
-		case LEFT:
-			nextY = y;
-			nextX = x - 1;
-			break;
-		case UP:
-			nextY = y - 1;
-			nextX = x;
-			break;
-		}
-
-		if (CanGo(nextY, nextX))
-		{
-			y = nextY;
-			x = nextX;
-			num++;
-		}
-		else
-		{
-			switch (dir)
-			{
-			case RIGHT:
-				dir = DOWN;
-				break;
-			case DOWN:
-				dir = LEFT;
-				break;
-			case LEFT:
-				dir = UP;
-				break;
-			case UP:
-				dir = RIGHT;
-				break;
-			}
-		}
-	}
-}
 
 int main()
 {
-	cin >> N;
-
-	SetBoard();
-
-	PrintBoard();
+	Test_1();
 
 	return 0;
 }
+
