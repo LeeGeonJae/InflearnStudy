614d6880ddf5f429ec8c3b5e1ed1bef290cb71a1
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-12-05 21:45:47 +0900

    다형성 - 동적 바인딩[virtual] - 정적 바인딩[일반 함수] -- 가상 함수 테이블(.vftable) - 순수 가상 함수 - 추상 클래스(순수 가상 함수 1개 이상 포함) )

--- main.cpp
+++ main.cpp
@@ -6,117 +6,99 @@ using namespace std;
 
 // 객체지향 (OOP Object Oriented Programing)
 // - 상속성
-// - 은닉성
+// - 은닉성 = 캡슐화
 // - 다형성
 
-// 은닉성(Data Hiding) = 캡슝화(Encapsulation)
-// 몰라도 되는 것은 깔끔하게 숨기겠다!
-// 숨기는 이유?
-// 1) 정말 위협하고 건드리면 안되는 경우
-// 2) 다른 경로로 접근하길 원하는 경우
+// 다형성(Polymorphism = Poly + morph) = 겉은 똑같은데, 기능이 다르게 동작한다
+// - 오버로딩(Overloading) = 함수 중복 정의 = 함수 이름의 재사용
+// - 오버라이딩(Overriding) = 재정의 = 부모 클래스의 함수를 자식 클래스에서 재정의
 
-// 자동차
-// - 핸들
-// - 페달
-// - 엔진
-// - 문
-// - 각종 전기선
+// 바인딩(Binding) = 묶는다
+// - 정적 바인딩(Static Binding) : 컴파일 시점에 결정
+// - 동적 바인딩(Dynamic Binding) : 실행 시점에 결정
 
-// 일반 구매자 입장에서 사용하는 것?
-// - 핸달/페달/문
-// 몰라도 됨 (오히려 건드리면 큰일남)
-// - 엔진, 각종 전기선
+// 일반 함수는 정적 바인딩을 사용
+// 동적 바인딩을 원한다면? -> 가상 함수 (virtual function)
 
-// public (공개적?) protected (보호받는?) private (개인적인?)
-// - public : 누구한테나 공개 << 아무나 사용하세요!
-// - protected : 나의 자손들한테만 허락
-// - private : 나만 사용할 것! << class Car 내부에서만 사용 가능!
+// 그런데 실제 객체가 어던 타입인지 어떻게 알고 알아서 가상함수를 호출해준걸까?
+// - 가상 함수 테이블 (vftable)
 
-// 상속 접근 지정자 : 다음 세대한테 부모님의 유산을 어떻게 물려줄지?
-// 부모님한테 물려받은 유산을 꼭 나의 자손들한테도 똑같이 물려줘야 하진 않음
-// - public : 공개적 상속? 부모님의 유산 설계 그대로~ (public -> public, protected -> protected)
-// - protected : 보호받는 상속? 내 자손들한테만 물려줄꺼야 (public -> protected, protected -> protected)
-// - private : 개인적인 상속? 나까지만 잘 쓰고 자손들한테는 아예 안 물려줄꺼야~ (public -> private, protected-> private)
+// .vftable [] 4바이트(32) 8바이트(64)
+// [VMove] [VDie] 
 
-class Car
+// 순수 가상 함수 : 구현은 없고 '인터페이스'만 전달하는 용도로 사용하고 싶을 경우
+// 추상 클래스 : 순수 가상 함수가 1개 이상 포함되면 바로 추상 클래스로 간주
+// - 직접적으로 객체를 만들 수 없게 됨
+
+class Player
 {
-public: // 멤버 접근 지정자
-	void MoveHandle() {}
-	void PushPadal() {}
-	void OpenDoor() {}
+public:
 
-	void TurnKey()
+	Player()
 	{
-		// ...
-		RunEngine();
+		_hp = 100;
 	}
 
-protected:
-	void DisassembleCar() {}	// 차를 분해한다.
-	void RunEngine() {}			// 엔진을 구동시킨다
-	void ConnecCircuit() {}		// 전기선 연결
+	void Move() { cout << "Move Player !" << endl; }
+	//void Move(int a) { cout << "Move Player (int) !" << endl; }
+
+	virtual void VMove() { cout << "VMove Player !" << endl; }
+	virtual void VDie() { cout << "VDie Player !" << endl; }
+
+	// 순수 가상 함수
+	virtual void Vattack() = 0;
 
 public:
-	// 핸들
-	// 페달
-	// 엔진
-	// 문
-	// 각종 전기선
+	int _hp;
 };
 
-class SuperCar : private Car // 상속 접근 지정자
+class Knight : public Player
 {
 public:
-	void PushRemoteController()
+
+	Knight()
 	{
-		RunEngine();
+		_stamina = 100;
 	}
+
+	void Move() { cout << "Move Knight Player !" << endl; }
+	
+	// 가상 함수는 재정의를 하더라도 가상 함수다!
+	virtual void VMove() { cout << "VMove Knight !" << endl; }
+	virtual void VDie() { cout << "VDie Knight !" << endl; }
+
+	virtual void Vattack() { cout << "Vattack Knight !" << endl; }
+
+public:
+	int _stamina;
 };
 
-class TestSuperCar : public SuperCar
+// [ [ Player] ]
+// [   Knight  ]
+
+void MovePlayer(Player* player)
 {
-public:
-	void Test()
-	{
-		// DisassembleCar();
-	}
-};
+	player->VMove();
+	player->VDie();
+}
 
-// [캡슐화]
-// 연관된 데이터와 함수를 논리적으로 묶어놓은 것
-
-class Berserker
+class Mage : public Player
 {
+
 public:
-
-	int GetHp() { return _hp; }
-
-	void SetHp(int hp)
-	{
-		_hp = hp;
-		if (_hp <= 50)
-			SetBerserkerMode();
-	}
-	// 사양 : 체력이 50 이하로 떨어지면 버서커 모드 발동 (강해짐)
-
-private:
-	void SetBerserkerMode()
-	{
-		cout << "매우 강해짐!" << endl;
-	}
-
-private:
-	int _hp = 100;
+	int _mp;
 };
 
 int main()
 {
-	TestSuperCar car;
-	car.PushRemoteController();
+	// Player p;		// 추상 클래스는 객체 생성이 불가능
+	// MovePlayer(&p);	// 플레이어는 플레이어다? YES
+	// MoveKnight(&p);	// 플레이어는 기사다? NO
 
-	Berserker b;
 
-	b.SetHp(20);
+	Knight k;
+	// MoveKnight(&k); // 기사는 기사다? YES
+	MovePlayer(&k); // 기사는 플레이어다? YES
 
 	return 0;
 }
