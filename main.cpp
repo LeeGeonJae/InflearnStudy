eddacd77d28d63db1de558bd57e3a4e0ad50353c
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-12-06 16:20:32 +0900

    연산자 오버로딩 (operator Overloading) - 멤버 연산자 함수 - 전역 연산자 함수 - 복사 대입 연산자 - 기타

--- main.cpp
+++ main.cpp
@@ -2,86 +2,155 @@
 
 using namespace std;
 
-// 오늘의 주제 : 초기화 리스트
+// 오늘의 주제 : 연산자 오버로딩(Operator Overloading)
 
-// 멤버 변수 초기화 ? 다양한 문법이 존재
+// 연산자 vs 함수
+// - 연산자는 피연산자의 개수/타입이 고정되어 있음
 
-// 초기화 왜 해야할까? 귀찮다~
-// - 버그 예방에 중요
-// - 포인터 등 주소값이 연루되어 있을 경우
+// 연산자 오버로딩?
+// 일단 [연산자 함수]를 정의해야 함
+// 함수도 멤버함수 vs 전역함수가 존재하는 것처럼, 연산자 함수도 두가지 방식으로 만들 수 있음
 
-// 초기화 방법
-// - 생성자 내에서
-// - 초기화 리스트
-// - C++11 문법
+// - 멤버 연산자 함수 version
+// -- a op b 형태에서 왼쪽을 기준으로 실행됨 (a가 클래스여야 가능. a를 '기준 피연산자'라고 함)
+// -- 한계) a가 클래스가 아니면 사용 못함
 
-// 초기화 리스트
-// - 일단 상속 관계에서 원하는 부모 생성자 호출할 때 필요하다
-// - 생성자 내에서 초기화 vs 초기화 리스트
-// -- 일반 변수는 별 차이 없음
-// -- 멤버 타입이 클래스인 경우 차이가 난다
-// -- 정의함과 동시에 초기화가 필요한 경우 (참조 타입, const 타입)
+// - 전역 연산자 함수 version
+// -- a op b 형태라면 a, b 모두를 연산자 함수의 피연산자로 만들어준다
 
-class Inventory
+// 그럼 무엇이 더 좋은가? 그런거 없음. 심지어 둘 중 하나만 지원하는 경우도 있기 때문.
+// - 대표적으로 대입 연산자 (a = b)는 전역 연산자 version으로는 못 만든다
+
+// 복사 대입 연산자
+// - 대입 연산자가 나온 김에 [복사 대입 연산자]에 대해 알아보자
+// 용어가 좀 헷갈린다 [복사 생성자] [대입 연산자] [복사 대입 연산자]
+// - 복사 대입 연산자 = 대입 연산자 중, 자기 자신의 참조 타입을 인자로 받는 것
+
+// 기타
+// - 모든 연산자를 다 오버로딩 할 수 있는 것은 아니다 (:: . .* 이런건 안됨)
+// - 모든 연산자가 다 2개 항이 있는 것 아님. ++ -- 가 대표적 (단항 연산자)
+// - 증감 연산자 ++ --
+// -- 전위형 (++a) operator++()
+// -- 후위형 (a++) operator++(int)
+
+class Position
 {
 public:
-	Inventory() { cout << "Inventory()" << endl; }
-	Inventory(int size) { cout << "Inventory(int size)" << endl; }
-
-	~Inventory() { cout << "~Inventory()" << endl; }
-
-public:
-	int _size = 10;
-};
-
-class Player
-{
-public:
-	Player() {}
-	Player(int id) {}
-
-};
-
-// Is-A (Knight Is-A Player? 기사는 플레이어다 ) OK -> 상속관계
-// Has-A (Knight Has-A Inventory? 기사는 인벤토리를 포함하고 있다 ) OK -> 포함관계
-
-class Knight : public Player
-{
-public:
-	Knight() : Player(1), _hp(100), _inventory(20), _hpRef(_hp), _hpConst(100)
-		/*
-		선처리 영역
-
-		_inventory = Inventory()
-
-		*/
+	Position operator+(const Position& arg)
 	{
-		_hp = 100;
-		// _inventory = Inventory(20);
+		Position pos;
+		pos._x = _x + arg._x;
+		pos._y = _y + arg._y;
+		return pos;
+	}
 
-		//-_hpRef = _hp;
-		//_hpConst = _hp;
+	Position operator+(int arg)
+	{
+		Position pos;
+		pos._x = _x + arg;
+		pos._y = _y + arg;
+		return pos;
+	}
+
+	bool operator==(const Position & arg)
+	{
+		return _x == arg._x && _y == arg._y;
+	}
+
+	Position& operator=(int arg)
+	{
+		_x = arg;
+		_y = arg;
+
+		// Position* this = 내 자신의 주소;
+		return *this;
+	}
+
+	// [복사 생성자] [복사 대입 연산자] 등 특별 대우를 받는 이유는,
+	// 말 그대로 객체가 '복사'되길 원하는 특징 때문
+	// TODO) 동적할당 시간에 더 자세히 알아볼 것
+	Position& operator=(const Position& arg)
+	{
+		_x = arg._x;
+		_y = arg._y;
+
+		// Position* this = 내 자신의 주소;
+		return *this;
+	}
+
+	Position& operator++()
+	{
+		_x++;
+		_y++;
+		return *this;
+	}
+
+	Position operator++(int)
+	{
+		Position ret = *this;
+		++_x;
+		++_y;
+		return ret;
 	}
 
 public:
-	int _hp; // 쓰레기 값
-	Inventory _inventory;
-
-	int& _hpRef;
-	const int _hpConst;
+	int _x;
+	int _y;
 };
 
+//void operator=(const Position& a, int b)
+//{
+//	a._x = b;
+//	a._y = b;
+//}
+
+Position operator+(int a, const Position& b)
+{
+	Position ret;
+
+	ret._x = b._x + a;
+	ret._y = b._y + a;
+
+	return ret;
+}
 
 int main()
 {
-	Knight k;
+	int a = 1;
+	int b = 2;
+	int c = ++(++a);
 
-	cout << k._hp << endl;
+	a++;
+	++a;
 
-	if (k._hp < 0)
-	{
-		cout << "Knight is Dead" << endl;
-	}
+	Position pos;
+	pos._x = 0;
+	pos._y = 0;
+
+	Position pos2;
+	pos2._x = 1;
+	pos2._y = 1;
+
+	Position pos3 = pos + pos2;
+	//pos3 = pos.operator+(pos2);
+
+	Position pos4 = pos3 + 1;
+	// pos4 = pos3.operator(1);
+
+	Position pos5 = 1 + pos3;
+	// 전역 변수로 만들면 왼쪽이 클래스가 아니더라도 오류가 발생하지 않는다.
+
+	bool isSame = (pos3 == pos4);
+
+	pos3 = (pos5 = 5);
+	// 전역 변수로 오류가 나는 이유는 
+	// 전역 변수로 만들 수 있게 하면 오른쪽에도 대입이 될 수 있는 형태가 될 수 있기 때문에 아주 위험해 문법적으로 막아두었다.
+	// 무조건 왼쪽 클래스에 대입이 될 수 있도록 멤버 연산자 함수로 만들어야 한다.
+
+	// (const Pos&)줘	(Pos)복사값 줄게
+	pos5 = pos3++;
+
+	++(++pos3);
 
 	return 0;
 }
