c8bddb12c38972f016bcb8dc06f282e35f506c10
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2022-12-31 23:02:30 +0900

    템플릿 기초

--- main.cpp
+++ main.cpp
@@ -2,106 +2,68 @@
 
 using namespace std;
 
-// 오늘의 주제 : 함수 객체
-
-void HelloWorld()
-{
-	cout << "Hello World" << endl;
-}
-
-void HelloNumber(int number)
-{
-	cout << "Hello Number " << number << endl;
-}
+// 오늘의 주제 : 템플릿 기초
 
 class Knight
 {
 public:
-	void AddHp(int addHp)
-	{
-		_hp += addHp;
-	}
+	// ...
 
-private:
+public:
 	int _hp = 100;
 };
 
-class Functor
+template<typename T>
+void Print(T a)
 {
-public:
-	void operator() ()
-	{
-		cout << "Functor Test" << endl;
-		cout << _value << endl;
-	}
+	cout << a << endl;
+}
 
-	bool operator()(int num)
-	{
-		cout << "Functor Test" << endl;
-		_value += num;
-		cout << _value << endl;
-
-		return true;
-	}
-
-private:
-	int _value = 0;
-};
-
-class MoveTesk
+// 템플릿 특수화
+template<>
+void Print(Knight a)
 {
-public:
-	void operator()()
-	{
-		// TODO
-		cout << "해당 좌표로 플레이어 이동" << endl;
-	}
+	cout << "Knight !!!!!!!!!!!" << endl;
+	cout << a._hp << endl;
+}
 
+template<typename T1, typename T2>
+void Print(T1 a, T2 b)
+{
+	cout << a << " " << b << endl;
+}
 
-public:
-	int _playerId;
-	int _posX;
-	int _posY;
-};
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
+// 연산자 오버로딩 (전역함수 버젼)
+ostream& operator<<(ostream& os, const Knight& k)
+{
+	os << k._hp;
+	return os;
+}
 
 int main()
 {
-	// 함수 객체 (Functor) : 함수처럼 동작하는 객체
-	// 함수 포인터의 단점
+	// 템플릿 : 함수나 클래스를 찍어내는 틀
+	// 1) 함수 템플릿
+	// 2) 클래스 템플릿
 
-	// 함수 포인터 선언
+	Print<int>(50);
+	Print(50.0f);
+	Print(50.0);
+	Print("Hello World");
 
-	void (*pfunc)(void);
+	Print("Hello ", 100);
 
-	// 동작을 넘겨줄 때 유용하다
-	pfunc = &HelloWorld;
-	(pfunc)();
+	int result1 = Add(1, 2);
+	float result2 = Add<float>(1.1f, 2.2f);
 
-	// 함수 포인터 단점
-	// 1) 시그니처가 안 맞으면 사용할 수 없다
-	// 2) 상태를 가질 수 없다
-	//pfunc = &HelloNumber;
-
-	// [함수처럼 동작] 하는 객체
-	// () 연산자 오버로딩
-	HelloWorld();
-
-	Functor functor;
-
-	functor();
-	bool ret = functor(3);
-
-	// MMO에서 함수 객체를 사용하는 예시
-	// 클라 <-> 서버
-	// 서버 : 클라가 보내준 네트워크 패킷을 받아서 처리
-	// ex) 클라 : 나 (5, 0) 좌표로 이동시켜줘!
-	MoveTesk tesk;
-	tesk._playerId = 100;
-	tesk._posX = 5;
-	tesk._posY = 0;
-
-	// 나중에 여유 될 때 일감을 실행한다.
-	tesk();
+	Knight k1;
+	Print(k1);
 
 	return 0;
 }
