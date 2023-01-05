8f07dd090ced21665d6e3f05c05f5729684a1220
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-05 19:03:52 +0900

    10 * 10 맵이 있다. Player는 { 1, 1 } 좌표에 위치해 있고 Goal은 맵에 랜덤으로 생성이 된다.
    게임이 시작하면 Player는 맵에 생성된 Goal로 자동으로 이동하게 코딩

--- main.cpp
+++ main.cpp
@@ -1,42 +1,45 @@
 #include <iostream>
-#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 
-const int ITEM_COUNT = 4;
-const int MAX_WEIGHT = 6;
+const int MAP_SIZE = 10;
+const int PLAYER_START_X = 1;
+const int PLAYER_START_Y = 1;
 
-struct Item {
-    int weight;
-    int price;
+struct Coordinate {
+    int x;
+    int y;
 };
 
-bool compareByPrice(const Item& i1, const Item& i2) {
-    return (float)i1.price / (float)i1.weight > (float)i2.price / (float)i2.weight;
-}
-
 int main() {
-    Item items[ITEM_COUNT] = {
-        {4, 6}, // Item 1: A knife weighing 4 is 6 won.
-        {3, 5}, // Item 2: An old item with a weight of 3 for 5 won.
-        {2, 3}, // Item 3: Apple with a weight of 2 for 3 won.
-        {1, 1}  // Item 4: 1 won fork.
-    };
+    // Seed the random number generator
+    srand(time(0));
 
-    // Sort the items by price in descending order
-    std::sort(items, items + ITEM_COUNT, compareByPrice);
+    Coordinate player = { PLAYER_START_X, PLAYER_START_Y };
+    Coordinate goal;
 
-    int totalWeight = 0;
-    int totalPrice = 0;
+    // Generate a random goal location
+    goal.x = rand() % MAP_SIZE;
+    goal.y = rand() % MAP_SIZE;
 
-    // Add as many copies of each item as possible
-    for (int i = 0; i < ITEM_COUNT; i++) {
-        while (totalWeight + items[i].weight <= MAX_WEIGHT) {
-            totalWeight += items[i].weight;
-            totalPrice += items[i].price;
+    while (player.x != goal.x || player.y != goal.y) {
+        if (player.x < goal.x) {
+            player.x++;
         }
+        else if (player.x > goal.x) {
+            player.x--;
+        }
+        if (player.y < goal.y) {
+            player.y++;
+        }
+        else if (player.y > goal.y) {
+            player.y--;
+        }
+
+        std::cout << "Player moved to: (" << player.x << ", " << player.y << ")" << std::endl;
     }
 
-    std::cout << "Total weight: " << totalWeight << std::endl;
-    std::cout << "Total price: " << totalPrice << std::endl;
+    std::cout << "Player reached the goal!" << std::endl;
 
     return 0;
 }
