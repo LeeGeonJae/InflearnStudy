059d7691d8a235b0ff9f4ff8601dad2e4fc2bd4a
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-06 14:42:53 +0900

    template #2 - 템플릿 기초 - 템플릿 특수화

--- main.cpp
+++ main.cpp
@@ -1,45 +1,68 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 
-const int MAP_SIZE = 10;
-const int PLAYER_START_X = 1;
-const int PLAYER_START_Y = 1;
+using namespace std;
 
-struct Coordinate {
-    int x;
-    int y;
+// 오늘의 주제 : 템플릿 기초
+
+// typename T를 붙이면 '조커카드' (어떤 타입도 다 넣을 수 있음)
+// 그런데 무조건 typename을 붙여야 하는 것은 아니다
+// template< > 안에 들어가는건 [골라줘야 하는 목록] 이라고 볼 수 있음
+template<typename T, int SIZE>
+class RandomBox
+{
+public:
+	T GetRandomData()
+	{
+		int idx = rand() % SIZE;
+		return _data[idx];
+	}
+
+public:
+	T _data[SIZE];
 };
 
-int main() {
-    // Seed the random number generator
-    srand(time(0));
+// 템플릿 특수화
+// double 버젼의 RandomBox가 호출이 되면 해당 클래스가 실행
+template<int SIZE>
+class RandomBox<double, SIZE>
+{
+public:
+	double GetRandomData()
+	{
+		cout << "RandomBox Double" << endl;
+		int idx = rand() % SIZE;
+		return _data[idx];
+	}
 
-    Coordinate player = { PLAYER_START_X, PLAYER_START_Y };
-    Coordinate goal;
+public:
+	double _data[SIZE];
+};
 
-    // Generate a random goal location
-    goal.x = rand() % MAP_SIZE;
-    goal.y = rand() % MAP_SIZE;
+int main()
+{
+	srand(static_cast<unsigned int>(time(nullptr)));
 
-    while (player.x != goal.x || player.y != goal.y) {
-        if (player.x < goal.x) {
-            player.x++;
-        }
-        else if (player.x > goal.x) {
-            player.x--;
-        }
-        if (player.y < goal.y) {
-            player.y++;
-        }
-        else if (player.y > goal.y) {
-            player.y--;
-        }
+	// 템플릿 : 함수나 클래스를 찍어내는 틀
+	// 1) 함수 템플릿
+	// 2) 클래스 템플릿
+	RandomBox<int, 10> rb1;
+	for (int i = 0; i < 10; i++)
+	{
+		rb1._data[i] = i;
+	}
+	int value1 = rb1.GetRandomData();
+	cout << value1 << endl;
 
-        std::cout << "Player moved to: (" << player.x << ", " << player.y << ")" << std::endl;
-    }
 
-    std::cout << "Player reached the goal!" << std::endl;
+	RandomBox<double, 20> rb2;
+	for (int i = 0; i < 20; i++)
+	{
+		rb2._data[i] = i + 0.5;
+	}
+	double value2 = rb2.GetRandomData();
+	cout << value2 << endl;
 
-    return 0;
+	//rb1 = rb2; // 같은 데이터 타입만 가능하다
+
+	return 0;
 }
