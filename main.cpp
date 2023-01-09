4b065ccab1cf22784b2909d7014ccd156938087b
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-10 01:09:09 +0900

    vector #3

--- main.cpp
+++ main.cpp
@@ -19,54 +19,39 @@ int main()
 	// 반복자(iterator) : 포인터와 유사한 개념. 컨테이너의 원소(데이터)를 가리키고, 다음/이전 원소로 이동 가능
 	vector<int> v(10);
 
+	v.reserve(1000);
 	for (vector<int>::size_type i = 0; i < v.size(); i++)
 		v[i] = i;
 
-	vector<int>::iterator it;
-	int* ptr;
+	// - 중간 삽입/삭제 (BAD)
+	// - 처음/끝 삽입/삭제 (BAD / GOOD)
+	// - 임의 접근 (Random Access)
 
-	it = v.begin();
-	ptr = &v[0];
+	// vector = 동적 배열 = 동적으로 커지는 배열 = 배열
+	// 원소가 하나의 메모리 블록에 연속하게 저장된다 !!!
 
-	cout << (*it) << endl;
-	cout << (*ptr) << endl;
+	// [                         ]
+	// [0] [1] [2] [3] [4] [5] []
 
-	ptr++;
-	++ptr;
-	it++;
-	++it;
+	v.push_back(1);
+	v.pop_back();
 
-	it += 2;
-	it = it - 2;
+	//vector<int>::iterator insertIt = v.insert(v.begin() + 2, 5);
+	//vector<int>::iterator eraseIt1 = v.erase(v.begin() + 2);
+	//vector<int>::iterator eraseIt2 = v.erase(v.begin() + 2, v.begin() + 4);
 
-	// 더 복잡해보이는데?
-	// 다른 컨테이너는 v[i]와 같은 인덱스 접근이 안될 수도 있음
-	// iterator는 vector뿐 아니라, 다른 컨테이너에도 공통적으로 있는 개념
-	vector<int>::iterator itBegin = v.begin();
-	vector<int>::iterator itEnd = v.end();
-	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+	// 쭉- 스캔을 하면서, 3이라는 데이터가 있으면 일괄 삭제하고 싶다
+
+	for (vector<int>::iterator it = v.begin(); it != v.end();)
 	{
-		cout << (*it) << endl;
+		int data = *it;
+		if (data == 3)
+			it = v.erase(it);
+		else
+		{
+			++it;
+		}
 	}
-	cout << "iterator" << endl << endl;
-
-	int* ptrBegin = &v[0]; // v.begin()._Ptr;
-	int* ptrEnd = ptrBegin + 10; // v.end()._Ptr;
-	for (int* ptr = ptrBegin; ptr != ptrEnd; ++ptr)
-	{
-		cout << (*ptr) << endl;
-	}
-	cout << "Ptr" << endl << endl;
-
-	// const int*;
-	//vector<int>::const_iterator cit1 = v.cbegin();
-	//*cit1 = 100;
-
-	for (vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it)
-	{
-		cout << (*it) << endl;
-	}
-	cout << "reverse_iterator" << endl;
 
 	return 0;
 }
