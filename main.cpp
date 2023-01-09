4a96707cb9faa5091b9ac7918632ea9a186ea672
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-09 22:27:22 +0900

    vector #2

--- main.cpp
+++ main.cpp
@@ -8,10 +8,6 @@ using namespace std;
 
 int main()
 {
-	// STL (Standard Tamplate Libary)
-	// 프로그래밍할 때 필요한 자료구조/알고리즘들을
-	// 템플릿으로 제공하는 라이브러리
-
 	// 컨테이터(Container) : 데이터를 저장하는 객체 (자료구조 Data Structure)
 
 	// vector (동적 배열)
@@ -20,41 +16,57 @@ int main()
 	// - 처음/끝 삽입/삭제
 	// - 임의 접근
 
-	// 동적 배열
-	// 중요한 개념 -> 어떤 마법을 부렸길래 배열을 '유동적으로' 사용한 것인가?
+	// 반복자(iterator) : 포인터와 유사한 개념. 컨테이너의 원소(데이터)를 가리키고, 다음/이전 원소로 이동 가능
+	vector<int> v(10);
 
-	// 1) (여유분을 두고) 메모리를 할당한다
-	// 2) 여유분까지 꽉 찼으면, 메모리를 증설한다
+	for (vector<int>::size_type i = 0; i < v.size(); i++)
+		v[i] = i;
 
-	// 1) 여유분은 얼만큼이 적당할까?
-	// 2) 증설을 얼만큼 해야 할까?
-	// 3) 기존의 데이터를 어떻게 처리할까?
+	vector<int>::iterator it;
+	int* ptr;
 
-	int arr[100];
+	it = v.begin();
+	ptr = &v[0];
 
-	vector<int> v(1000, 0);
-	vector<int> v2 = v;
+	cout << (*it) << endl;
+	cout << (*ptr) << endl;
 
-	// [                 ]
-	//v.resize(1000);
-	cout << v.size() << " " << v.capacity() << endl;
-	// size(실제 사용 데이터 개수)
-	// 1 2 3 4 5 6 7 ...
-	
-	//v.reserve(1000);
-	// capacity (여유분을 포함한 용량 개수)
-	// 1 2 3 4 6 9 13 19 28 42 63
+	ptr++;
+	++ptr;
+	it++;
+	++it;
 
-	for (int i = 0; i < 1001; i++)
+	it += 2;
+	it = it - 2;
+
+	// 더 복잡해보이는데?
+	// 다른 컨테이너는 v[i]와 같은 인덱스 접근이 안될 수도 있음
+	// iterator는 vector뿐 아니라, 다른 컨테이너에도 공통적으로 있는 개념
+	vector<int>::iterator itBegin = v.begin();
+	vector<int>::iterator itEnd = v.end();
+	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
 	{
-		//v[i] = 100;
-		v.push_back(100);
-		cout << v.size() << " " << v.capacity() << endl;
+		cout << (*it) << endl;
 	}
+	cout << "iterator" << endl << endl;
 
-	v.clear();
-	vector<int>().swap(v); // () : 깡통이니까 메모리를 날려주고 이 과정이 끝나면 깡통도 메모리가 없어진다
-	cout << v.size() << " " << v.capacity() << endl;
+	int* ptrBegin = &v[0]; // v.begin()._Ptr;
+	int* ptrEnd = ptrBegin + 10; // v.end()._Ptr;
+	for (int* ptr = ptrBegin; ptr != ptrEnd; ++ptr)
+	{
+		cout << (*ptr) << endl;
+	}
+	cout << "Ptr" << endl << endl;
+
+	// const int*;
+	//vector<int>::const_iterator cit1 = v.cbegin();
+	//*cit1 = 100;
+
+	for (vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it)
+	{
+		cout << (*it) << endl;
+	}
+	cout << "reverse_iterator" << endl;
 
 	return 0;
 }
