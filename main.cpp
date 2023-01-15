5d3104f1207ddf4e38700ec1e552ab4170afc215
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-15 20:23:10 +0900

    오른값(rvalue) 참조와 std::move - 이동 생성자 - 이동 대입 연산자 - 원본을 다른 포인터로 이동하고 기존의 포인터 삭제

--- main.cpp
+++ main.cpp
@@ -8,47 +8,103 @@
 
 using namespace std;
 
-// 오늘의 주제 : 우측 참조
+// 오늘의 주제 : 오른값(rvalue) 참조와 std::move
 
-class Creature
+class Pet
 {
-public:
-	
+
 };
 
-class Player : public Creature
+class Knight
 {
 public:
-	virtual void Attack() final
+	Knight()
 	{
-		cout << "Player!" << endl;
-	}
-};
-
-class Knight : public Player
-{
-public:
-	// 재정의 (override)
-	virtual void Attack() override
-	{
-		cout << "Knight!" << endl;
+		cout << "Knight()" << endl;
 	}
 
-	// 오버로딩 (overloading) : 함수 이름의 재사용
-	void Attack(int a)
+	// 복사 생성자
+	Knight(const Knight& knight)
+	{
+		cout << "const Knight&" << endl;
+	}
+
+	// 이동 생성자
+	Knight(Knight&& knight)
 	{
 
 	}
 
-private:
-	int _stamina = 100;
+	virtual ~Knight()
+	{
+		if (_pet)
+			delete _pet;
+	}
+
+	// 복사 대입 연산자
+	void operator=(const Knight& knight)
+	{
+		cout << "operator=(const Knight&)" << endl;
+
+		_hp = knight._hp;
+
+		if (knight._pet)
+			_pet = new Pet(*(knight._pet));
+	}
+
+	// 이동 대입 연산자
+	void operator=(Knight&& knight) noexcept
+	{
+		cout << "operator=(Knight&& knight)" << endl;
+
+		_hp = knight._hp;
+		_pet = knight._pet;
+
+		knight._pet = nullptr;
+	}
+
+public:
+	int _hp = 100;
+	Pet* _pet = nullptr;
 };
+
+void TestKnight_Copy(Knight knight) {  }
+void TestKnight_lValueRef(Knight& knight) {  }
+void TestKnight_ConstlValueRef(const Knight& knight) {  }
+void TestKnight_RValueRef(Knight&& knight) {  } // 이동 대상!!
 
 int main()
 {
-	Player* player = new Knight();
+	// 왼값(lvalue) vs 오른값(rvalue)
+	// - lvalue : 단일식을 넘어서 계속 지속되는 개체
+	// - rvalue : lvalue가 아닌 나머지 (임시 값, 열거형, 람다, i++ 등)
 
-	player->Attack();
+	int a = 3;
+
+	Knight k1;
+
+	TestKnight_Copy(k1);
+
+	TestKnight_lValueRef(k1);
+	//TestKnight_lValueRef(Knight());
+
+	TestKnight_ConstlValueRef(Knight());
+
+	TestKnight_RValueRef(static_cast<Knight&&>(k1));
+
+	Knight k2;
+	k2._pet = new Pet();
+	k2._hp = 1000;
+
+	// 원본은 날려도 된다 << 는 Hint를 주는 쪽에 가깝다!
+	Knight k3;
+	//k3 = static_cast<Knight&&>(k2);
+
+	k3 = std::move(k2); // 오른값 참조로 캐스팅
+	// std::move의 본래 이름 후보 중 하나가 rvalue_cast
+
+	std::unique_ptr<Knight> uptr = std::make_unique<Knight>();
+	std::unique_ptr<Knight> uptr2 = std::move(uptr);
 
 	return 0;
 }
